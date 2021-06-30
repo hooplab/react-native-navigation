f8a7c437f00e155a6ba356b9affd2a6b137ab0fc
Lars Lone Rasmussen <lars@hoopla.no>
2021-06-30 16:46:05 +0200

    Strip other screens to provoke bug

--- playground/src/app.ts
+++ playground/src/app.ts
@@ -30,63 +30,27 @@ function start() {
 function setRoot() {
   Navigation.setRoot({
     root: {
-      bottomTabs: {
+      stack: {
         children: [
           {
-            stack: {
-              children: [
-                {
-                  component: {
-                    name: 'Layouts',
-                  },
-                },
-              ],
-              options: {
-                bottomTab: {
-                  text: 'Layouts',
-                  icon: require('../img/layouts.png'),
-                  selectedIcon: require('../img/layouts_selected.png'),
-                  testID: testIDs.LAYOUTS_TAB,
-                },
-              },
-            },
-          },
-          {
-            stack: {
-              children: [
-                {
-                  component: {
-                    name: 'Options',
-                  },
-                },
-              ],
-              options: {
-                topBar: {
-                  title: {
-                    text: 'Default Title',
-                  },
-                },
-                bottomTab: {
-                  text: 'Options',
-                  icon: require('../img/options.png'),
-                  selectedIcon: require('../img/options_selected.png'),
-                  testID: testIDs.OPTIONS_TAB,
-                },
-              },
-            },
-          },
-          {
-            stack: {
-              children: [
-                {
-                  component: {
-                    name: 'Navigation',
-                  },
-                },
-              ],
+            component: {
+              name: 'Options',
             },
           },
         ],
+        options: {
+          topBar: {
+            title: {
+              text: 'Default Title',
+            },
+          },
+          bottomTab: {
+            text: 'Options',
+            icon: require('../img/options.png'),
+            selectedIcon: require('../img/options_selected.png'),
+            testID: testIDs.OPTIONS_TAB,
+          },
+        },
       },
     },
   });
